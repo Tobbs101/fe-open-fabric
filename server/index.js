28362abe2987a4e8aa33de91818d65a190bc595a
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-26 23:51:11 +0100

    Update index

--- server/index.js
+++ server/index.js
@@ -13,4 +13,19 @@ app.get("/", (req, res) => {
   res.send("Welcome to app...");
 });
 
+const CONNECTION_URL = "mongodb://localhost:27017";
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}...`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+mongoose.set("strictQuery", true);
