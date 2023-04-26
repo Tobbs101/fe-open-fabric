b74a5f1e23942731ac6677bc8c4a4a74e01a7887
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-26 23:46:27 +0100

    Update index

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to app...");
+});
+
+const PORT = process.env.PORT || 5000;
