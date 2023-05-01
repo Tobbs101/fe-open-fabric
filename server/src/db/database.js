d1dd593c79c6ea51f5d14bbc7c2660c1f9b80104
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-01 20:22:57 +0100

    Update app

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -14,15 +14,15 @@
 
 //heroku config
 module.exports = {
-  HOST: "us-cdbr-east-06.cleardb.net",
-  USER: "b0746e53ac8ce9",
-  PASSWORD: "660d9211",
-  DB: "heroku_7167f9aaa257fa5",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "heroku_7167f9aaa257fa6",
   dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
