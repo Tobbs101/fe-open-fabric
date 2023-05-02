4e476ba50450b665a4d4202ba86681ce4f2c89c5
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-02 16:25:43 +0100

    Update products

--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -10,6 +10,10 @@ interface Product {
   description: string;
   price: number;
 }
+
+interface ProductResponse {
+  product: Product[];
+}
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -17,28 +21,37 @@ interface Product {
 })
 export class ProductsComponent {
   public products: Array<Product> = [];
-  public userData:any;
-  constructor(private router: Router, private http: HttpClient,private dataService:DataService) {
-    this.dataService.getData().subscribe(data => {
+  public userData: any;
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private dataService: DataService
+  ) {
+    this.dataService.getData().subscribe((data) => {
       this.userData = data;
     });
   }
 
   ngOnInit(): void {
     const token = sessionStorage.getItem('token');
-    this.http.get<Array<Product>>(`${baseUrl}/api/v1.0/product/all`,{
-      headers: {'Content-Type': 'application/json',Authorization: `Bearer ${token}`}
-    }).subscribe(
-      (data) => {
-        this.products = data;
-        console.log(data);
-      },
-      (error) => {
-        console.log('Error fetching products:', error);
-        if(error.status === 401){
-          this.router.navigate(['/'])
+    this.http
+      .get<ProductResponse>(`${baseUrl}/api/v1.0/product/all`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .subscribe(
+        (data) => {
+          this.products = data.product;
+          console.log('p', this.products);
+        },
+        (error) => {
+          console.log('Error fetching products:', error);
+          if (error.status === 401) {
+            this.router.navigate(['/']);
+          }
         }
-      }
-    );
+      );
   }
 }
