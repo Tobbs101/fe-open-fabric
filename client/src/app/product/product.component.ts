a1007bffbeb11b5d7893ae2cda3ae10dc5904fcd
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-02 19:05:59 +0100

    Update Product Component

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+interface Product {
+  id: number;
+  productName: string;
+  productDescription: string;
+  productPrice: number;
+}
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  private Product: Object = {};
+  public Product: Product = {id:0,productName:'',productDescription:'',productPrice:0};
 
   constructor(private router: Router){}
 
