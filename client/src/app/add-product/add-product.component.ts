fce8f4c85cece82435b6cc2d9536e7758b06df70
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-05-02 17:00:03 +0100

    Update Add Product

--- client/src/app/add-product/add-product.component.ts
+++ client/src/app/add-product/add-product.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormBuilder,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import {baseUrl} from '../../url';
 
 @Component({
   selector: 'app-add-product',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent {
+  newProduct: FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+    this.newProduct = this.formBuilder.group({
+      productName:['', Validators.required],
+      productDescription:['', Validators.required],
+      productPrice:['', Validators.required],
+      productAvailable:['', Validators.required]
+    })
+  }
+
+  
 
 }
